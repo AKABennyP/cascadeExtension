da6a940e047bf349f0e0e19c5af38efdd92c9ace
benjaminMaze <akabennyp@gmail.com>
2024-10-04 20:34:27 -0400

    Adding range selection

--- src/cascade-content.js
+++ src/cascade-content.js
@@ -1,25 +1,39 @@
 "use strict"
 
-var cssBlock = "";
+var cssBlockName = "cssBlock";
+var cssBlock = '';
 var applied = true;
 
-const addCssBlock = () => {
+const addCssBlock = (cssBlock) => {
+    //temp
+    var ranges = ['cssBlock', 'cssBlock2'];
+    chrome.storage.sync.set({'ranges': ranges});
+    //temp
     var style = document.createElement('style');
     style.innerHTML = cssBlock;
     document.head.appendChild(style);
 };
 
-const keys = ['applied', 'cssBlock'];
-chrome.storage.sync.get(keys, (data) => {
-    if(data.applied === false) {
-        applied = false;
+chrome.storage.sync.get('selectedRange', (data) => {
+    if(data.selectedRange) {
+        cssBlockName = data.selectedRange;
     }
-    if(data.cssBlock) {
-        cssBlock = data.cssBlock;
-    }
-    if(applied && cssBlock.length > 0) {
-        window.addEventListener('load', (event) => {
-            addCssBlock();
-        });
-    }
+
+    const keys = ['applied', cssBlockName];
+    chrome.storage.sync.get(keys, (data) => {
+        if(data.applied === false) {
+            applied = false;
+        }
+        if(data[cssBlockName]) {
+            cssBlock = data[cssBlockName];
+        }
+        if(applied && cssBlock.length > 0) {
+            window.addEventListener('load', (event) => {
+                addCssBlock(cssBlock);
+            });
+            window.addEventListener('resize', function() {
+                addCssBlock(cssBlock);
+            });
+        }
+    });
 });
--- src/cascade.js
+++ src/cascade.js
@@ -4,7 +4,7 @@ const setBageText = (applied) => {
 };
 
 const checkbox = document.querySelector('input.apply');
-chrome.storage.sync.get('applied', (data) => {
+chrome.storage.sync.get(['applied'], (data) => {
     checkbox.checked = !!data.applied;
     setBageText(data.applied);
 });
@@ -13,14 +13,47 @@ checkbox.addEventListener('change', (event) => {
         chrome.storage.sync.set({'applied': event.target.checked});
         setBageText(event.target.checked);
     }
-})
-
-const input = document.querySelector('#cssBlock');
-chrome.storage.sync.get('cssBlock', (data) => {
-    input.value = data.cssBlock || "";
 });
+
+const rangeSelect = document.querySelector('select.rangeSelect');
+const input = document.querySelector('#cssBlock');
+var cssBlockName = 'cssBlock';
+rangeSelect.addEventListener('change', (event) => {
+    if(event.target instanceof HTMLSelectElement) {
+        chrome.storage.sync.set({'selectedRange': event.target.value});
+        chrome.storage.sync.get(event.target.value, (data) => {
+            input.value = data[event.target.value];
+        });
+    }
+});
+chrome.storage.sync.get(['selectedRange'], (data) => {
+    cssBlockName = data.selectedRange || 'cssBlock';
+    chrome.storage.sync.get(cssBlockName, (cssData) => {
+        input.value = cssData.cssBlock;
+    });
+});
+
 input.addEventListener('change', (event) => {
     if(event.target instanceof HTMLTextAreaElement) {
-        chrome.storage.sync.set({'cssBlock': event.target.value});
+        chrome.storage.sync.get(['selectedRange'], (data) => {
+            var cssBlockName = data.selectedRange || 'cssBlock';
+            var blockObject = {};
+            blockObject[cssBlockName] = event.target.value;
+            chrome.storage.sync.set(blockObject);
+        });
     }
-})
+});
+
+chrome.storage.sync.get(['ranges'], (data) => {
+    if(data.ranges) {
+        data.ranges.forEach(range => {
+            const opt = document.createElement("option");
+            if(range === cssBlockName) {
+                opt.selected = true;
+            }
+            opt.text = range;
+            opt.value = range;
+            rangeSelect.add(opt);
+        });
+    }
+});
